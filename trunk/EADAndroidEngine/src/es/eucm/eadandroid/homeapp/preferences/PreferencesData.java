30888842cd7f78027b6fefc543d8bf756ce358a9
rtornero@e-ucm.es <rtornero@e-ucm.es@8e06ce1c-cfad-11de-a306-d16691e8d855>
2011-07-07 11:09:39 +0000

    Removing bad projects from trunk

--- trunk/EADAndroidEngine/src/es/eucm/eadandroid/homeapp/preferences/PreferencesData.java
+++
@@ -1,8 +0,0 @@
-package es.eucm.eadandroid.homeapp.preferences;
-
-public class PreferencesData {
-
-	public static String default_repository = "http://eadventure-android.googlecode.com/files/clasificadorjuegos.xml";
-	
-	
-}
